4e059fa2706bfb63f193b015eac0f1fbd2f17c35
Alessandro Manfredi <alle.manfredi@gmail.com>
2023-02-20 14:22:40 +0100

    refactor(scripts): updates deploy-test-environment

--- scripts/test/deploy-test-environment.js
+++ scripts/test/deploy-test-environment.js
@@ -1,13 +1,15 @@
 const { ethers, upgrades } = require('hardhat')
 const { getRole } = require('../../test/utils/index')
+const { time } = require('@nomicfoundation/hardhat-network-helpers')
 
 const ACL_ADDRESS = '0xFDcae423E5e92B76FE7D1e2bcabd36fca8a6a8Fe'
 const EPOCH_DURATION = 60 * 60 * 24 * 15
-const STAKING_MANAGER_ADDRESS = '0xeb10e80D99655B51E3a981E888a73D0B21e21A6C'
 const PNT_ADDRESS = '0x89Ab32156e46F46D02ade3FEcbe5Fc4243B9AAeD'
+const PBTC_ADDRESS = '0x62199B909FB8B8cf870f97BEf2cE6783493c4908'
 const TOKEN_MANAGER_ADDRESS = '0xD7E8E79d318eCE001B39D83Ea891ebD5fC22d254'
 const DAO_ROOT_ADDRESS = '0x6Ae14ff8d24F719a8cf5A9FAa2Ad05dA7e44C8b6'
 const PNT_HOLDER_1_ADDRESS = '0xaeaa8c6ebb17db8056fa30a08fd3097de555f571'
+const PBTC_HOLDER_1_ADDRESS = '0x0a3b24e917192fb3238d118bfa331cfad5a07368'
 const LEND_MAX_EPOCHS = 24
 const MINIMUM_BORROWING_FEE = 0.3 * 10 ** 6 // 30%
 
@@ -15,6 +17,16 @@ const main = async () => {
   const signer = await ethers.getSigner()
   const daoRoot = await ethers.getImpersonatedSigner(DAO_ROOT_ADDRESS)
   const pntHolder1 = await ethers.getImpersonatedSigner(PNT_HOLDER_1_ADDRESS)
+  const pbtcHolder1 = await ethers.getImpersonatedSigner(PBTC_HOLDER_1_ADDRESS)
+
+  await signer.sendTransaction({
+    to: pntHolder1.address,
+    value: ethers.utils.parseEther('1')
+  })
+  await signer.sendTransaction({
+    to: pbtcHolder1.address,
+    value: ethers.utils.parseEther('1')
+  })
 
   const ACL = await ethers.getContractFactory('ACL')
   const ERC20 = await ethers.getContractFactory('ERC20')
@@ -24,10 +36,15 @@ const main = async () => {
   const RegistrationManager = await ethers.getContractFactory('RegistrationManager')
   const FeesManager = await ethers.getContractFactory('FeesManager')
 
-  console.info('Deploying ...')
-
   const acl = await ACL.attach(ACL_ADDRESS)
   const pnt = await ERC20.attach(PNT_ADDRESS)
+  const pbtc = await ERC20.attach(PBTC_ADDRESS)
+
+  console.log('Funding signer ...')
+  await pnt.connect(pntHolder1).transfer(signer.address, await pnt.balanceOf(pntHolder1.address))
+  await pbtc.connect(pbtcHolder1).transfer(signer.address, await pbtc.balanceOf(pbtcHolder1.address))
+
+  console.info('Deploying ...')
 
   const stakingManager = await upgrades.deployProxy(StakingManager, [PNT_ADDRESS, TOKEN_MANAGER_ADDRESS], {
     initializer: 'initialize',
@@ -41,7 +58,7 @@ const main = async () => {
 
   const borrowingManager = await upgrades.deployProxy(
     BorrowingManager,
-    [PNT_ADDRESS, STAKING_MANAGER_ADDRESS, epochsManager.address, LEND_MAX_EPOCHS],
+    [PNT_ADDRESS, stakingManager.address, epochsManager.address, LEND_MAX_EPOCHS],
     {
       initializer: 'initialize',
       kind: 'uups'
@@ -50,7 +67,7 @@ const main = async () => {
 
   const registrationManager = await upgrades.deployProxy(
     RegistrationManager,
-    [PNT_ADDRESS, STAKING_MANAGER_ADDRESS, epochsManager.address, borrowingManager.address],
+    [PNT_ADDRESS, stakingManager.address, epochsManager.address, borrowingManager.address],
     {
       initializer: 'initialize',
       kind: 'uups'
@@ -59,7 +76,7 @@ const main = async () => {
 
   const feesManager = await upgrades.deployProxy(
     FeesManager,
-    [STAKING_MANAGER_ADDRESS, epochsManager.address, borrowingManager.address, registrationManager.address, MINIMUM_BORROWING_FEE],
+    [stakingManager.address, epochsManager.address, borrowingManager.address, registrationManager.address, MINIMUM_BORROWING_FEE],
     {
       initializer: 'initialize',
       kind: 'uups'
@@ -73,9 +90,51 @@ const main = async () => {
   console.log('Assigning roles ...')
   await borrowingManager.grantRole(getRole('BORROW_ROLE'), registrationManager.address)
   await borrowingManager.grantRole(getRole('DEPOSIT_INTEREST_ROLE'), signer.address)
+  await borrowingManager.grantRole(getRole('DEPOSIT_INTEREST_ROLE'), signer.address)
 
-  console.log('Transferring PNT to test address ...')
-  await pnt.connect(pntHolder1).transfer(signer.address, ethers.utils.parseEther('500000'))
+  console.log('Lending ...')
+  await pnt.approve(borrowingManager.address, '0xffffffffffffffffffffffffffffffffffffffff')
+  await borrowingManager.lend('400000000000000000000000', EPOCH_DURATION * 10, signer.address)
+
+  console.log('Creating sentinel ...')
+  await pnt.approve(registrationManager.address, '0xffffffffffffffffffffffffffffffffffffffff')
+
+  console.log('Depositing interests and fees ...')
+  await pnt.approve(feesManager.address, '0xffffffffffffffffffffffffffffffffffffffff')
+  await pbtc.approve(feesManager.address, '0xffffffffffffffffffffffffffffffffffffffff')
+
+  // epoch 1
+  await time.increase(EPOCH_DURATION)
+
+  for (let i = 0; i < 3; i++) {
+    await feesManager.depositFee(pnt.address, '2000000000000000000000')
+  }
+
+  // epoch 2
+  await time.increase(EPOCH_DURATION)
+  for (let i = 0; i < 3; i++) {
+    await feesManager.depositFee(pnt.address, '2000000000000000000000')
+    await feesManager.depositFee(pbtc.address, '200000000000000000')
+  }
+
+  // epoch 3
+  await time.increase(EPOCH_DURATION)
+  for (let i = 0; i < 3; i++) {
+    await feesManager.depositFee(pnt.address, '1500000000000000000000')
+    await feesManager.depositFee(pbtc.address, '300000000000000000')
+  }
+
+  // epoch 4
+  await time.increase(EPOCH_DURATION)
+  for (let i = 0; i < 3; i++) {
+    await feesManager.depositFee(pbtc.address, '500000000000000000')
+  }
+
+  // epoch 5
+  await time.increase(EPOCH_DURATION)
+  await registrationManager.updateSentinelRegistrationByBorrowing(4, '0x')
+
+  await time.increase(EPOCH_DURATION * 10)
 
   console.log(
     JSON.stringify({
@@ -83,8 +142,7 @@ const main = async () => {
       borrowingManager: borrowingManager.address,
       epochsManager: epochsManager.address,
       registrationManager: registrationManager.address,
-      feesManager: feesManager.address,
-      signer: signer.address
+      feesManager: feesManager.address
     })
   )
 }
