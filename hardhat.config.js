3c925713186f0a8736560fff6908fdfc55bc391d
Alessandro Manfredi <alle.manfredi@gmail.com>
2023-05-02 08:29:50 +0200

    feat(tasks): adds decode-forwarder-metadata task

--- hardhat.config.js
+++ hardhat.config.js
@@ -79,3 +79,5 @@ module.exports = {
     timeout: 100000000
   }
 }
+
+require('./tasks/decode-forwarder-metadata.js')
---
+++ tasks/decode-forwarder-metadata.js
@@ -0,0 +1,28 @@
+task('decode-forwarder-metadata', 'Decode the pNetwork Forwarder Metadata')
+  .addParam('metadata', 'The pNetwork Metadata', undefined, types.string, true)
+  .setAction(async (_taskArgs, _hre) => {
+    const { ethers } = _hre
+    const { metadata } = _taskArgs
+
+    const abiCoder = new ethers.utils.AbiCoder()
+    const [version, userData, sourceNetworkId, senderAddress, destinationNetworkId, receiverAddress, protocolOptions, protocolReceipt] =
+      abiCoder.decode(['bytes1', 'bytes', 'bytes4', 'address', 'bytes4', 'address', 'bytes', 'bytes'], metadata)
+
+    const [callsAndTargets, originAddress, callerAddress] = abiCoder.decode(['bytes', 'address', 'address'], userData)
+    // const [targets, data] = abiCoder.decode(['address[]', 'bytes[]'], callsAndTargets)
+
+    console.log({
+      version,
+      userData: {
+        callsAndTargets,
+        originAddress,
+        callerAddress
+      },
+      sourceNetworkId,
+      senderAddress,
+      destinationNetworkId,
+      receiverAddress,
+      protocolOptions,
+      protocolReceipt
+    })
+  })
